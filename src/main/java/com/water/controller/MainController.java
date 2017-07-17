eced19d7be14d4317849ab5b43e63963a9fe0988
151250209 <151250209@smail.nju.edu.cn>
2017-07-17 12:11:22 +0800

    修改了com.water中的几个文件

--- src/main/java/com/water/controller/MainController.java
+++ src/main/java/com/water/controller/MainController.java
@@ -1,8 +1,8 @@
 package com.water.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.water.entity.User;
-import com.water.service.UserService;
+//import com.water.entity.User;
+//import com.water.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,26 +17,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
     //测试
     @Autowired
-    private UserService userService;
+//    private UserService userService;
 
     @RequestMapping(value = "saveUser", method = RequestMethod.GET)
     @ResponseBody
     public String saveUser() {
-        userService.saveUser();
+//        userService.saveUser();
         return "success!";
     }
 
     @RequestMapping(value = "saveUser2")
     @ResponseBody
     public Object saveUser2(String Id, String Password,String Name,String TelephoneNumber, String HomeAddress,String PostCode) {
-        Long userId = userService.saveUser(Id,Password,Name,TelephoneNumber,HomeAddress,PostCode);
+//        Long userId = userService.saveUser(Id,Password,Name,TelephoneNumber,HomeAddress,PostCode);
         JSONObject obj = new JSONObject();
-        if (userId != null && userId > 0) {
-            obj.put("result", 1);
-            obj.put("obj", userId);
-        } else {
-            obj.put("result", 0);
-        }
+//        if (userId != null && userId > 0) {
+//            obj.put("result", 1);
+//            obj.put("obj", userId);
+//        } else {
+//            obj.put("result", 0);
+//        }
 
         return obj;
     }
@@ -44,14 +44,14 @@ public class MainController {
     @RequestMapping(value = "getUser")
     @ResponseBody
     public Object getUser(long id) {
-        User user = userService.getById(id);
+//        User user = userService.getById(id);
         JSONObject obj = new JSONObject();
-        if (user != null) {
-            obj.put("result", 1);
-            obj.put("obj", user);
-        } else {
-            obj.put("result", 0);
-        }
+//        if (user != null) {
+//            obj.put("result", 1);
+//            obj.put("obj", user);
+//        } else {
+//            obj.put("result", 0);
+//        }
         return obj;
     }
 }
