539cfbdaff4472aa139c55e77ac880db44def6a6
151250209 <151250209@smail.nju.edu.cn>
2017-07-18 10:44:13 +0800

    新增UserDao

---
+++ src/main/java/com/water/dao/Impl/UserDaoImpl.java
@@ -0,0 +1,17 @@
+package com.water.dao.Impl;
+
+import com.water.dao.UserDao;
+
+/**
+ * Created by 朱晨乾 on 2017/7/18.
+ */
+public class UserDaoImpl implements UserDao{
+    /**
+     * 验证身份
+     * @param idUser
+     * @param password
+     */
+    public void testIdentify(String idUser, String password) {
+
+    }
+}
---
+++ src/main/java/com/water/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.water.dao;
+
+/**
+ * Created by 朱晨乾 on 2017/7/18.
+ */
+public interface UserDao {
+    public void testIdentify(String idUser, String password);
+}
