0cad8f2651fa358a13d87d3038143007dea88f18
151250188 <151250188@smail.nju.edu.cn>
2017-07-25 17:02:14 +0800

    图片路径

--- src/main/java/com/water/controller/MainController.java
+++ src/main/java/com/water/controller/MainController.java
@@ -4,12 +4,16 @@ package com.water.controller;
 import com.water.service.ApplyService;
 import com.water.service.UploadService;
 import com.water.service.UserService;
+
+import java.io.File;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
