a314408d7e3d6ad87c184a884851a071dca39d8b
151250162 <151250162@smail.nju.edu.cn>
2017-07-28 17:55:15 +0800

    project v4(no picture uploading)

--- src/main/java/com/water/controller/ProjectController.java
+++ src/main/java/com/water/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.water.controller;
 
 import com.water.entity.Project;
 import com.water.service.ProjectService;
+import net.sf.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class ProjectController {
@@ -32,34 +34,64 @@ public class ProjectController {
         project.setName(head);
         project.setDescription(body);
 
-        //long id=projectService.saveProject(project);
-        System.out.println("success");
-        return 0;
+        long id=projectService.saveProject(project);
+
+        return id;
+//        return 0;
     }
 
     /**
-     * @param request
      * @param response
-     * @return void
+     * @return 项目列表
      * @throws Exception
      */
-    @RequestMapping("/all")
+    @RequestMapping("/allProject")
     @ResponseBody
-    public void getAll(HttpServletRequest request,HttpServletResponse response) throws IOException {
+    public void getAll(HttpServletResponse response) throws IOException {
+        List<Project> projects=projectService.findAllProjects();
 
+        System.out.println(projects.get(0).getName());
+        JSONArray jsonObject=JSONArray.fromObject(projects);
+
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().print(jsonObject);
+        response.getWriter().flush();
     }
 
     /**
      * @param request
-     * @param response
      * @return boolean
      * @throws Exception
      */
-    @RequestMapping("/delete")
+    @RequestMapping("/deleteProject")
     @ResponseBody
-    public boolean delete(HttpServletRequest request,HttpServletResponse response) throws IOException {
-        return false;
+    public boolean delete(HttpServletRequest request) throws IOException {
+        String id=request.getParameter("id");
+        boolean success=projectService.deleteProject(Long.parseLong(id));
+        return success;
+//        return true;
     }
 
+    /**
+     * @param request
+     * @return boolean
+     * @throws Exception
+     */
+    @RequestMapping("/modifyProject")
+    @ResponseBody
+    public boolean modify(HttpServletRequest request) throws IOException {
+        long id=Long.parseLong(request.getParameter("id"));
+        String head=request.getParameter("headline");
+        String body=request.getParameter("body");
 
+        Project project=new Project();
+        project.setIdProject(id);
+        project.setName(head);
+        project.setDescription(body);
+
+        boolean success=projectService.updateProject(project);
+
+        return success;
+//        return true;
+    }
 }
--- src/main/webapp/resources/js/Admin_Work_Project.js
+++ src/main/webapp/resources/js/Admin_Work_Project.js
@@ -3,7 +3,21 @@ var content={};
 
 //加载界面使用
 function load(){
-
+    var url="/allProject";
+    $.ajax({
+        type:"Post",
+        url:url,
+        async:true,
+        success:function (data) {
+            var objects=$.parseJSON(data);
+            for(var i=0;i<data.length;i++){
+                var id=objects[i].idProject;
+                var headline=objects[i].name;
+                var markupstr=objects[i].description;
+                addOne(id,headline,markupstr);
+            }
+        }
+    })
 }
 
 function publish(){
@@ -13,36 +27,51 @@ function publish(){
     }
     else {
         var markupStr = $('#summernote').summernote('code');
-        var url="/publish";
-        $.ajax({
-            type:"POST",
-            url:url,
-            async:true,
-            data:{"headline":headline,"body":markupStr},
-            dataType:"json",
-            success:function (data){
-                // head.push(headline);
-                // content.push(markupStr);
-                head[data]=headline;
-                content[data]=markupStr;
+        var active=$("#scro1").find(".active");
+        var id=active.eq(0).attr('id');
+        if(id!=="add"){
+            var url="/modifyProject";
+            $.ajax({
+                type:"POST",
+                url:url,
+                async:true,
+                data:{"id":id,"headline":headline,"body":markupStr},
+                dataType:"json",
+                success:function (data) {
+                    if(data=="false"){
+                        alert("修改失败！请重试！");
+                    }
+                    else{
+                        head[id]=headline;
+                        content[id]=markupStr;
+                        alert("修改成功！");
+                    }
+                }
+            })
+        }
+        else{
+            var url="/publish";
+            $.ajax({
+                type:"POST",
+                url:url,
+                async:true,
+                data:{"headline":headline,"body":markupStr},
+                dataType:"json",
+                success:function (data){
+                    // head.push(headline);
+                    // content.push(markupStr);
+                    if(parseInt(data)===-1){
+                        alert("发布失败！请重试！");
+                    }
+                    else{
+                        addOne(data,headline,markupStr);
 
-                var list=document.getElementById("scro1");
-                var item=document.createElement("li");
-                item.setAttribute("id",data);
-                item.setAttribute("onclick","javascript:show("+data+");");
-
-                var a=document.createElement("a");
-                a.innerHTML=headline;
-                item.appendChild(a);
-                var span=document.createElement("span");
-                span.setAttribute("class","fa fa-angle-right");
-                item.appendChild(span);
-
-                list.appendChild(item);
-
-                deleteAll();
-            }
-        });
+                        alert("发布成功！");
+                        deleteAll();
+                    }
+                }
+            });
+        }
     }
 }
 
@@ -60,7 +89,7 @@ function show(id){
     document.getElementById(id).setAttribute("class","active");
 
     var cancel = document.getElementById("cancel");
-    if(id=="add"){
+    if(id==="add"){
         document.getElementById("headline").value="";
         $('#summernote').summernote('code', "");
 
@@ -79,9 +108,47 @@ function show(id){
 
 //to be continued
 function del(id){
+    var url="/deleteProject";
+    $.ajax({
+        type:"POST",
+        url:url,
+        async:true,
+        data:{"id":id},
+        dataType:"json",
+        success:function (data){
+            if(data==="false"){
+                alert("删除失败！请重试！");
+            }
+            else{
+                var node=document.getElementById(id);
+                node.parentNode.removeChild(node);
+
+                delete head[id];
+                delete content[id];
+
+                alert("删除成功！");
+                show("add");
+            }
+        }
+    })
+}
+
+function addOne(id,headline,markupStr) {
+    head[id]=headline;
+    content[id]=markupStr;
+
     var list=document.getElementById("scro1");
-    //list.removeChild(list.childNodes[id+1]);
-    delete head[id];
-    delete content[id];
+    var item=document.createElement("li");
+    item.setAttribute("id",id);
+    item.setAttribute("onclick","javascript:show("+id+");");
+
+    var a=document.createElement("a");
+    a.innerHTML=headline;
+    item.appendChild(a);
+    var span=document.createElement("span");
+    span.setAttribute("class","fa fa-angle-right");
+    item.appendChild(span);
+
+    list.appendChild(item);
 }
 
