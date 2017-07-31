7f8de5bdb2191185194e4fc74844854f66fec029
zhanglei <151250193@smail.nju.edu.cn>
2017-07-31 10:22:31 +0800

    修改

--- src/main/java/com/water/service/Impl/UploadServiceImpl.java
+++ src/main/java/com/water/service/Impl/UploadServiceImpl.java
@@ -39,15 +39,7 @@ public class UploadServiceImpl implements UploadService {
         uploadDao.save(sample);
     }
 
-    public boolean addUpload(Long idSample, Double longitude, Double latitude, Date sampleDate, Double volume, String image, String remark) {
-        Sample sampleEntity = new Sample();
-        sampleEntity.setIdSample(idSample);
-        sampleEntity.setSampleDate(sampleDate);
-        sampleEntity.setVolume(volume);
-        //        sampleEntity.setImage(image);
-        sampleEntity.setRemark(remark);
-        return uploadDao.save(sampleEntity);
-    }
+
 
     public boolean addUpload(Sample sample) {
         return uploadDao.save(sample);
--- src/main/java/com/water/service/UploadService.java
+++ src/main/java/com/water/service/UploadService.java
@@ -13,8 +13,6 @@ public interface UploadService {
 
     public boolean updateSample(long idSample, int state);
 
-    public boolean addUpload(Long idSample, Double longitude, Double latitude, Date sampleDate, Double volume, String image, String remark);
-
     public boolean addUpload(Sample sample);
 
     public Sample searchSample(Long idSample);
@@ -26,7 +24,6 @@ public interface UploadService {
 
     public void addTxt(Sample sample);
 
-   // public ArrayList<Sample> filterSample(String eventID,String startDate,String endDate);
-   public int judgeByID(long id);
+    public int judgeByID(long id);
 
 }
