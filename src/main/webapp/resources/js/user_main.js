135548304584b3875608f402d817f5dd0d3e6e98
151250162 <151250162@smail.nju.edu.cn>
2017-08-01 14:57:02 +0800

    link

--- src/main/webapp/resources/js/user_main.js
+++ src/main/webapp/resources/js/user_main.js
@@ -8,22 +8,28 @@ function load() {
         url:url,
         async:true,
         success:function (data) {
-            var objects=data;
-            // var objects=JSON.parse(data);
-            var headline=document.getElementById("proName");
-            var body=document.getElementById("proContent");
-            var time=document.getElementById("proDate");
+            if(data==null){
+                var headline=document.getElementById("proName");
+                headline="暂无项目";
+            }
+            else {
+                var objects=data;
+                // var objects=JSON.parse(data);
+                var headline=document.getElementById("proName");
+                var body=document.getElementById("proContent");
+                var time=document.getElementById("proDate");
 
-            name=data.name;
-            var content=getText(data.description).substring(0,301)+'...';
-            var year=parseInt(data.date['year'])+1900;
-            var month=parseInt(data.date['month'])+1;
-            var day=data.date['date'];
-            var date=year+'-'+month+'-'+day;
+                name=data.name;
+                var content=getText(data.description).substring(0,301)+'...';
+                var year=parseInt(data.date['year'])+1900;
+                var month=parseInt(data.date['month'])+1;
+                var day=data.date['date'];
+                var date=year+'-'+month+'-'+day;
 
-            headline.innerHTML=name;
-            body.innerHTML=content;
-            time.innerHTML=date;
+                headline.innerHTML=name;
+                body.innerHTML=content;
+                time.innerHTML=date;
+            }
         }
     })
 }
